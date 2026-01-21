0a0d10d34d20df31095be8bab063df040089f778
DragostinH <dragostin.hristov@gmail.com>
2026-01-21 14:18:20 +0200

    add: FrontEnd setup with Vue done

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<style scoped></style>
+
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="border-2 flex items-center h-10/12 bg-conic-330">You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
